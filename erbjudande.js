90089fac07872686b76578a2ef85ee46139ad38a
id21wjt <123098094+id21wjt@users.noreply.github.com>
2025-09-06 19:54:15 +0200

    Update erbjudande.js

--- erbjudande.js
+++ erbjudande.js
@@ -3,11 +3,7 @@ function visaSlumpErbjudande() {
   const slumpIndex = Math.floor(Math.random() * produkter.length);
   const erbjudandeProdukt = produkter[slumpIndex];
 
-  // Ändrar rubrik på sidan
-  const rubrik = document.querySelector('p');
-  rubrik.textContent = "Dagens erbjudande: " + erbjudandeProdukt + " 20% rabatt!";
-  rubrik.style.color = "red";
-}
+
 
 // Kör automatiskt när sidan laddas
 window.onload = visaSlumpErbjudande;
