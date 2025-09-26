3d9e09692edbfef8387994062e92c031a74ff982
id21wjt <123098094+id21wjt@users.noreply.github.com>
2025-09-26 15:54:42 +0200

    Update bigPic.js

--- bigPic.js
+++ bigPic.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   $(".product-img").hover(
     function () {
-      $(this).animate({ width: "150px", height: "150px" }, 300); // större
+      $(this).animate({ width: "150px", height: "150px" }, 300); 
     },
     function () {
-      $(this).animate({ width: "50px", height: "50px" }, 300);   // tillbaka
+      $(this).animate({ width: "50px", height: "50px" }, 300);   
     }
   );
 });
